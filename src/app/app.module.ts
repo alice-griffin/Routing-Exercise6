c140e2b7d7e9245d58381272c443f2a7e3ad08c1
unknown <alicekaygriffin@gmail.com>
2020-07-18 18:53:06 -0400

    add cats and dogs routing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { DogsComponent } from '../app/dogs/dogs.component'
+import { CatsComponent } from '../app/cats/cats.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component'
+
+const appRoutes: Routes = [
+  {path: 'dogs', component: DogsComponent},
+  {path: 'cats', component: CatsComponent},
+  {path: '', redirectTo: 'cats', pathMatch: 'full'},
+  {path: '**', component: PagenotfoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +21,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
